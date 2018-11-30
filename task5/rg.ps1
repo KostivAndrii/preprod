07f41429dd917d3ddf4c054f0baa815272994293
Andrii Kostiv <Andrii_Kostiv@epam.com>
2018-11-30 16:04:58 +0200

    task5.final+RG

--- task5/rg.ps1
+++ task5/rg.ps1
@@ -1,11 +1,18 @@
-$resourceGroupName = 'RG001'
+$resourceGroupName = 'RG0012'
 $location = 'Korea South'
 
 #New-AzureRmDeployment -Name demoEmptyRG$(Get-Random -Minimum 100 -Maximum 999) -Location $location `
 #  -TemplateFile task5n.json -rgName $resourceGroupName -rgLocation $location
+New-AzureRmDeployment `
+  -Name job1 `
+  -Location $location `
+  -TemplateFile rg.json `
+  -rgName $resourceGroupName `
+  -rgLocation $location `
+  -Verbose
+
  
-New-AzureRmResourceGroupDeployment -Name job$(Get-Random -Minimum 100 -Maximum 999) `
- -TemplateFile task5.json -ResourceGroupName $resourceGroupName
+New-AzureRmResourceGroupDeployment -Name job2 -TemplateFile task5.json -ResourceGroupName $resourceGroupName -Verbose
 
 #for ($i=1; $i -le 2; $i++)
 #{
