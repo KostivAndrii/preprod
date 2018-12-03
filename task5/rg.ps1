35f5ab2010b6223ac2192cb1bd2f9d2eb8858234
Andrii Kostiv <Andrii_Kostiv@epam.com>
2018-12-03 16:39:18 +0200

    task5 finish. D2s_v3

--- task5/rg.ps1
+++ task5/rg.ps1
@@ -1,10 +1,10 @@
-$resourceGroupName = 'RG0012'
-$location = 'Korea South'
+$resourceGroupName = 'RG001'
+$location = 'France Central'
+$FilePath = 'task5.json'
+$parametersFilePath = 'task5.param.json'
 
-#New-AzureRmDeployment -Name demoEmptyRG$(Get-Random -Minimum 100 -Maximum 999) -Location $location `
-#  -TemplateFile task5n.json -rgName $resourceGroupName -rgLocation $location
 New-AzureRmDeployment `
-  -Name job1 `
+  -Name job0$(Get-Random -Minimum 10 -Maximum 99) `
   -Location $location `
   -TemplateFile rg.json `
   -rgName $resourceGroupName `
@@ -12,10 +12,10 @@ New-AzureRmDeployment `
   -Verbose
 
  
-New-AzureRmResourceGroupDeployment -Name job2 -TemplateFile task5.json -ResourceGroupName $resourceGroupName -Verbose
+New-AzureRmResourceGroupDeployment  `
+  -Name job1$(Get-Random -Minimum 10 -Maximum 99)  `
+  -TemplateFile $FilePath  `
+  -TemplateParameterFile $parametersFilePath  `
+  -ResourceGroupName $resourceGroupName  `
+  -Verbose
 
-#for ($i=1; $i -le 2; $i++)
-#{
-#      Invoke-AzureRmVMRunCommand -ResourceGroupName $resourceGroupName -Name $resourceGroupName'_VM'$i -CommandId 'RunPowerShellScript' `
-#           -ScriptPath "customiis.ps1" -Parameter @{"arg1" = $i} -AsJob
-#}
